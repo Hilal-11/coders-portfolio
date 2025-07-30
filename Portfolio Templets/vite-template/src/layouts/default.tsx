caa1f2145b23d2b7afce21a23fe45b658319ce4b
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-07-31 02:04:31 +0530

    changes 4

--- Portfolio Templets/vite-template/src/layouts/default.tsx
+++ Portfolio Templets/vite-template/src/layouts/default.tsx
@@ -8,12 +8,12 @@ export default function DefaultLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative flex flex-col h-screen">
+    <div className="relative flex flex-col min-h-screen">
       <Navbar />
-      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
+      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16 pb-8">
         {children}
       </main>
-      <footer className="w-full flex items-center justify-center py-3">
+      <footer className="w-full flex items-center justify-center py-3 mt-auto">
         
       </footer>
     </div>
