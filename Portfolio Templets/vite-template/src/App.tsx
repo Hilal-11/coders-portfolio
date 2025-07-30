3c6ac0e45b748ecd928a456377b6da87b8c05b99
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-07-31 02:03:27 +0530

    changes 1

--- Portfolio Templets/vite-template/src/App.tsx
+++ Portfolio Templets/vite-template/src/App.tsx
@@ -1,20 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 
 import IndexPage from "@/pages/index";
-import DocsPage from "@/pages/docs";
-import PricingPage from "@/pages/pricing";
-import BlogPage from "@/pages/blog";
-import AboutPage from "@/pages/about";
+import Projects from "./components/Projects Section/Projects";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
-      <Route element={<PricingPage />} path="/pricing" />
-      <Route element={<BlogPage />} path="/blog" />
-      <Route element={<AboutPage />} path="/about" />
-    </Routes>
+    <div className="bg-white dark:bg-black">
+      <div>
+        <IndexPage />
+      </div>
+      {/* Projects Secession */}
+      <div className="w-full bg-slate-50">
+          <Projects />
+      </div>
+    </div>
   );
 }
 
