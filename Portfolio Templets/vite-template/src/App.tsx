87fa6212c0f04e3dc784357243e018d2e255d3d2
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-02 02:19:44 +0530

    about secession

--- Portfolio Templets/vite-template/src/App.tsx
+++ Portfolio Templets/vite-template/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 
 import IndexPage from "@/pages/index";
 import Projects from "./components/Projects Section/Projects";
+import About from "./pages/AboutMe";
 
 function App() {
   return (
@@ -12,6 +13,10 @@ function App() {
       {/* Projects Secession */}
       <div className="w-full bg-slate-50">
           <Projects />
+      </div>
+      {/* About Section */}
+      <div className="w-full dark:bg-black bg-slate-50">
+          <About />
       </div>
     </div>
   );
