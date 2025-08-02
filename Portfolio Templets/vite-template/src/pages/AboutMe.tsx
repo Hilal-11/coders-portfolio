649afb8be575f16ae0ec8ad8580b08fdf4d1f71c
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-03 04:45:17 +0530

    sitedataa

--- Portfolio Templets/vite-template/src/pages/AboutMe.tsx
+++ Portfolio Templets/vite-template/src/pages/AboutMe.tsx
@@ -33,9 +33,6 @@ export default function DocsPage() {
             <div className="flex justify-center items-center py-4 px-5">
               <button 
                 className='dark:bg-neutral-900 ring-1 dark:ring-slate-800 ring-slate-400 cursor-pointer text-center w-full py-3 rounded-full bg-white shadow-lg poppins-bold text-sm flex gap-3 justify-center items-center'
-                // variants={buttonVariants}
-                // whileHover="hover"
-                // whileTap="tap"
               >
               <span className='text-2xl'><MdArrowOutward /></span>
                 Contect me
@@ -44,13 +41,23 @@ export default function DocsPage() {
           </div>
         </div>
 
-        <div className="lg:col-span-2 w-full mx-auto ">
-            <div className="py-10 px-2 lg:px-10 poppins-regular text-sm lg:text-lg">
+        <div className="lg:col-span-2 w-full mx-auto relative z-50 overflow-hidden">
+
+        <div className="flex justify-center gap-10 items-center absolute top-[100%] left-1/2 -z-10">
+          <span className=" block w-[700px] h-[700px] rounded-full shadow-lg shadow-slate-950 dark:shadow-green-500 blur-[1px] absolute"></span>
+          <span className=" block w-[600px] h-[600px] rounded-full shadow-lg shadow-slate-950 dark:shadow-green-500 blur-[1px] absolute"></span>
+          <span className=" block w-[500px] h-[500px] rounded-full shadow-lg shadow-slate-950 dark:shadow-green-500 blur-[1px] absolute"></span>
+          <span className=" block w-[400px] h-[400px] rounded-full shadow-lg shadow-slate-950 dark:shadow-green-500 blur-[1px] absolute"></span>
+          <span className=" block w-[300px] h-[300px] rounded-full shadow-lg shadow-slate-950 dark:shadow-green-500 blur-[1px] absolute"></span>
+          <span className=" block w-[200px] h-[200px] rounded-full shadow-lg shadow-slate-950 dark:shadow-green-500 blur-[1px] absolute"></span>
+          <span className=" block w-[100px] h-[100px] rounded-full shadow-lg shadow-slate-950 dark:shadow-green-500 blur-[1px] absolute"></span>
+        </div>
+
+        <div className="py-10 px-2 lg:px-10 poppins-regular text-sm lg:text-lg">
               <p>I'm Bark Aten, a passionate Web Designer & Developer based in the dynamic city of Berlin, Germany. I blend creative design with precise technical execution to deliver outstanding digital experiences. Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
 
             <div className="flex gap-3 lg:gap-5 justify-evenly flex-wrap lg:px-10 py-6 lg:h-auto h-[260px] overflow-hidden">
-
                   {techIcons.map((skill) => {
                     const IconComponent = skill.icon;
                     return (
@@ -59,13 +66,37 @@ export default function DocsPage() {
                       </div>
                     );
                   })}
-
             </div>
+
+            <div className="lg:px-10 flex gap-3 lg:gap-5 flex-wrap justify-evenly py-10">
+                <div className="shadow-[inset_-12px_-8px_40px_rgba(230,200,200,0.125)] lg:px-12 px-8 lg:py-3 py-2 rounded-lg flex justify-center items-center dark:bg-neutral-950 bg-slate-100  poppins-regular lg:text-sm text-[12px]">
+                  <p>Frontend Dev</p>
+                </div>
+                <div className="shadow-[inset_-12px_-8px_40px_#46464620] lg:px-12 px-8 lg:py-3 py-2 rounded-lg flex justify-center items-center dark:bg-neutral-950 bg-slate-100 poppins-regular lg:text-sm text-[12px]">
+                  <p>Backend Dev</p>
+                </div>
+                <div className="shadow-[inset_-12px_-8px_40px_#46464620] lg:px-12 px-8 lg:py-3 py-2 rounded-lg flex justify-center items-center dark:bg-neutral-950 bg-slate-100 poppins-regular lg:text-sm text-[12px]">
+                  <p>Devops</p>
+                </div>
+                <div className="shadow-[inset_-12px_-8px_40px_#46464620] lg:px-12 px-8 lg:py-3 py-2 rounded-lg flex justify-center items-center dark:bg-neutral-950 bg-slate-100 poppins-regular lg:text-sm text-[12px]">
+                  <p>AI Integration</p>
+                </div>
+                <div className="shadow-[inset_-12px_-8px_40px_#46464620] lg:px-12 px-8 lg:py-3 py-2 rounded-lg flex justify-center items-center dark:bg-neutral-950 bg-slate-100 poppins-regular lg:text-sm text-[12px]">
+                  <p>UI/UX Designer</p>
+                </div>
+                <div className="shadow-[inset_-12px_-8px_40px_#46464620] lg:px-12 px-8 lg:py-3 py-2 rounded-lg flex justify-center items-center dark:bg-neutral-950 bg-slate-100 poppins-regular lg:text-sm text-[12px]">
+                  <p>Data Analytics</p>
+                </div>
+                <div className="shadow-[inset_-12px_-8px_40px_#46464620] lg:px-12 px-8 lg:py-3 py-2 rounded-lg flex justify-center items-center dark:bg-neutral-950 bg-slate-100 poppins-regular lg:text-sm text-[12px]">
+                  <p>React Native Dev</p>
+                </div>
+            </div>
+
+              
+
+
         </div>
-
-
       </div>
-    <br /><br /><br /><br /><br />
     </div>
   );
 }
