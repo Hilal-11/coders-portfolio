bfcc50bfe9a294a87062796331e7a00ecd9864be
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-03 04:44:22 +0530

    app.tsx

--- Portfolio Templets/vite-template/src/App.tsx
+++ Portfolio Templets/vite-template/src/App.tsx
@@ -3,7 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import IndexPage from "@/pages/index";
 import Projects from "./components/Projects Section/Projects";
 import About from "./pages/AboutMe";
-
+import Footer from "./pages/Footer";
+import Contect from "./pages/Contect";
 function App() {
   return (
     <div className="bg-white dark:bg-black">
@@ -14,9 +15,20 @@ function App() {
       <div className="w-full bg-slate-50">
           <Projects />
       </div>
+      {/* CONTECT PAGE */}
       {/* About Section */}
       <div className="w-full dark:bg-black bg-slate-50">
           <About />
+      </div>
+
+      <div>
+        <Contect />
+      </div>
+
+
+    {/* FOOTER */}
+      <div className="">
+        <Footer />
       </div>
     </div>
   );
