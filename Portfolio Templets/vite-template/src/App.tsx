93f0cb036886420beb7c9343a6bc28b37a06cbac
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-04 16:54:25 +0530

    package

--- Portfolio Templets/vite-template/src/App.tsx
+++ Portfolio Templets/vite-template/src/App.tsx
@@ -1,36 +1,42 @@
 import { Route, Routes } from "react-router-dom";
-
 import IndexPage from "@/pages/index";
-import Projects from "./components/Projects Section/Projects";
 import About from "./pages/AboutMe";
 import Footer from "./pages/Footer";
 import Contect from "./pages/Contect";
+import Projects from "./components/Projects Section/Projects";
+import ProjectsPreview from './pages/Projects/ProjectsPreview'
+import DefaultLayout from "./layouts/default";
 function App() {
   return (
-    <div className="bg-white dark:bg-black">
-      <div>
-        <IndexPage />
-      </div>
-      {/* Projects Secession */}
-      <div className="w-full bg-slate-50">
-          <Projects />
-      </div>
-      {/* CONTECT PAGE */}
-      {/* About Section */}
-      <div className="">
-          <About />
-      </div>
-
-      <div>
-        <Contect />
-      </div>
+    <>
 
 
-    {/* FOOTER */}
-      <div className="">
-        <Footer />
-      </div>
-    </div>
+          <Routes>
+            <Route path="/" element={
+              <>
+                  <DefaultLayout>
+                    <IndexPage />
+                  </DefaultLayout>
+                  <Projects />
+                  <About/>
+                  <Contect />
+              </>
+            }></Route>
+          </Routes>
+
+          <Routes>
+            <Route path="/projects" element={
+              <>
+                <DefaultLayout>
+                  <ProjectsPreview />
+                </DefaultLayout>
+              </>
+            }></Route>
+          </Routes>
+
+          <Footer />
+    </>
+    
   );
 }
 
