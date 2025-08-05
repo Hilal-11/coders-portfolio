93648972f887ed3adf20e452190f32c8ba69a937
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-05 16:31:18 +0530

    experiances dummy json just for rendering on UI Frontend

--- Portfolio Templets/vite-template/src/pages/Experiance.tsx
+++ Portfolio Templets/vite-template/src/pages/Experiance.tsx
@@ -1,25 +1,82 @@
-import React from "react";
+
 // import {Tabs, Tab, Card, CardBody} from "@heroui/react";
 import {Tabs, Tab} from "@heroui/tabs";
 import {Card, CardBody} from "@heroui/card";
-
+import experianceData from '@/config/experiancesJson'
 function Experiance() {
-    const [isVertical, setIsVertical] = React.useState(true);
-
+    console.log(experianceData)
   return (
-    <div id='experiance-section' className='w-full lg:w-[70%] mx-auto h-auto py-20 px-2'>
+    <div id='experiance-section' className='w-full lg:w-[70%] mx-auto h-auto py-10 px-2'>
         <div className="lg:py-20">
             <div className="">
                 <h1 className='text-5xl lg:text-7xl poppins-bold text-center  '>Work Experience</h1>
-                <p className='text-center text-lg poppins-regular px-4 py-2 lg:px-28'>With hands-on experience across the full development stack, I've built robust, scalable, and modern web applications. Here's a breakdown of my key roles and contributions. I switch a lot of companies. It's mostly about the culture.</p>
+                <p className='text-center text-lg poppins-regular px-2 py-2 lg:px-28'>With hands-on experience across the full development stack, I've built robust, scalable, and modern web applications. Here's a breakdown of my key roles and contributions. I switch a lot of companies. It's mostly about the culture.</p>
             </div>
         {/* Vertical Tab View of experiances on doifferent companies and startups */}
-        <div className="mx-auto py-10 flex justify-center flex-col px-0 my-6 ">
-                <div className="flex w-full flex-col">
-                    <Tabs aria-label="Options" isVertical={isVertical}>
+            <div className="flex w-full flex-col my-6 mx-auto">
+                <Tabs aria-label="Options" >
+                    <Tab className="poppins-medium" key="Frontend Development" title="Frontend Development">
+                    <Card className="dark:bg-neutral-950">
+                        <CardBody className="dark:bg-neutral-950 poppins-regular text-[15px]">
+                        {experianceData[0].experianceDiscaription}
+                        </CardBody>
+                    </Card>
+                    </Tab>
+
+                <Tab className="poppins-medium" key={experianceData[1].experianceSkillOption} title={experianceData[1].experianceSkillOption}>
+                    <Card className="dark:bg-neutral-950">
+                        <CardBody className="dark:bg-neutral-950 poppins-regular text-[15px]">
+                        {experianceData[1].experianceDiscaription}
+                        </CardBody>
+                    </Card>
+                </Tab> 
+
+                <Tab className="poppins-medium" key={experianceData[2].experianceSkillOption} title={experianceData[2].experianceSkillOption}>
+                    <Card className="dark:bg-neutral-950">
+                        <CardBody className="dark:bg-neutral-950 poppins-regular text-[15px]">
+                        {experianceData[2].experianceDiscaription}
+                        </CardBody>
+                    </Card>
+                </Tab> 
+                <Tab className="poppins-medium" key={experianceData[3].experianceSkillOption} title={experianceData[3].experianceSkillOption}>
+                    <Card className="dark:bg-neutral-950">
+                        <CardBody className="dark:bg-neutral-950 poppins-regular text-[15px]">
+                        {experianceData[3].experianceDiscaription}
+                        </CardBody>
+                    </Card>
+                </Tab> 
+
+                <Tab className="poppins-medium" key={experianceData[4].experianceSkillOption} title={experianceData[4].experianceSkillOption}>
+                    <Card className="dark:bg-neutral-950">
+                        <CardBody className="dark:bg-neutral-950 poppins-regular text-[15px]">
+                        {experianceData[4].experianceDiscaription}
+                        </CardBody>
+                    </Card>
+                </Tab> 
+
+
+                <Tab className="poppins-medium" key={experianceData[5].experianceSkillOption} title={experianceData[5].experianceSkillOption}>
+                    <Card className="dark:bg-neutral-950">
+                        <CardBody className="dark:bg-neutral-950 poppins-regular text-[15px]">
+                        {experianceData[5].experianceDiscaription}
+                        </CardBody>
+                    </Card>
+                </Tab> 
+
+                </Tabs>
+            </div>
+
+
+
+
+
+
+        {/* <div className="mx-auto py-10 flex justify-center flex-col px-0 my-6 ">
+                <div className="flex w-full flex-col space-y-10">
+                    <Tabs className="" aria-label="Options" isVertical={isVertical}>
                     <Tab key="Frontend Development" title="Frontend Development">
                         <Card>
-                        <CardBody>
+                        <CardBody className="dark:bg-neutral-900">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
                             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
                             exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -44,7 +101,7 @@ function Experiance() {
                         </Card>
                     </Tab>
 
-                    <Tab key="AI Integration in AApplications" title="AI Integration in AApplications">
+                    
                         <Card>
                         <CardBody>
                             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
@@ -63,7 +120,7 @@ function Experiance() {
 
                     </Tabs>
             </div>
-            </div>
+            </div> */}
         </div>
     </div>
   )
