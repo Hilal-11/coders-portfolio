54217b284f78079fec72fe714436d5fa3da10984
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-06 22:11:41 +0530

    done

--- Portfolio Templets/vite-template/vite.config.ts
+++ Portfolio Templets/vite-template/vite.config.ts
@@ -5,7 +5,22 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
-   server: {
-    host: true
-  }
+
+  build: {
+    chunkSizeWarningLimit: 1000, // Set higher limit
+    // OR: Use manual chunks for better code splitting
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          ui: ['@radix-ui/react-icons', 'lucide-react'],
+          // split large libs
+        },
+      },
+    },
+  },
+
+
 });
+
+
