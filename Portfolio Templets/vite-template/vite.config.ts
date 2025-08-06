ffd3bf07109f5482be2e41a56f87278c0c4321aa
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-06 22:23:21 +0530

    ...

--- Portfolio Templets/vite-template/vite.config.ts
+++ Portfolio Templets/vite-template/vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  plugins: [react(), tsconfigPaths(), tailwindcss()]
 });
 
 
